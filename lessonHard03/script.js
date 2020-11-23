c62f88f6bfbeb7cb24d95a4b6b158c556e043784
Egor Matveev <egormsm@gmail.com>
2020-11-23 15:34:53 +0300

    Закончил 3 усложненное

--- lessonHard03/script.js
+++ lessonHard03/script.js
@@ -1,15 +1,29 @@
+'use strict';
 
-let num = 266219;
-let str = String(num);
-let arr = str.split('');
+let lang = prompt("Введите ru или en");
+let arr = [];
+    if (lang === "ru") {
+        arr = ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"];
+    }
+    if (lang === "en") {
+        arr = ["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
+    }
+console.log(arr);
+switch (lang){
+    case "ru":
+        arr = ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"];
+        break;
+    case "en":
+        arr = ["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
+        break;
+}
+console.log(arr);
+    arr = {
+    "ru":["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"],
+    "en":["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"]
+};
+console.log(arr[lang]);
 
-let result = arr.reduce(function(a,b) {
-    "use strict";
-    return a*b;
-});
- 
-console.log(result);
-
-let deg = result ** 3;
-let degstr = String(deg);
-console.log(degstr.substring(0,2));
+ let namePerson = prompt("Выберите Артем или Максим?");
+ namePerson = namePerson === "Артем" ? console.log('Директор') : namePerson === "Максим" ? 
+    console.log("Преподаватель") : console.log('Студент');
