3d3a579dadc2d0121d84146bc35e4105a89c95bb
Egor Matveev <egormsm@gmail.com>
2020-11-24 14:43:16 +0300

    Закончил выполнение 4го усложненного

--- lessonHard04/script.js
+++ lessonHard04/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function edit(b){
+    if (typeof b !== 'string'){
+        alert("значение не является строкой");
+        return;
+    } 
+    b = b.trim();
+    return b.length > 30 ? b.slice(0,30) + '...' : b;
+}
