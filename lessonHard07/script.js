3e0d41db77c3fbc30a18a0825aee07fb11a2ac64
Egor Matveev <egormsm@gmail.com>
2020-12-06 01:52:31 +0300

    Закончил усложненное 9 задание

--- lessonHard07/script.js
+++ lessonHard07/script.js
@@ -1,18 +1,23 @@
-"use strict";
-let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
-  today = new Date().getDay() - 1;
+'use strict';
 
-for (let i = 0; i < week.length; i++) {
-  if (i === today) {
-    if (week[i] === 'суббота' || week[i] === 'воскресенье') {
-      document.write(`<p><b><i>${week[i]}</i></b></p>`);
-    } else {
-      document.write(`<p><b>${week[i]}</b></p>`);
-    }
-  } else if (week[i] === 'суббота' || week[i] === 'воскресенье') {
-    document.write(`<p><i>${week[i]}</i></p>`);
-  } else {
-    document.write(`<p>${week[i]}</p>`);
-  }
-}
-console.log(week);
+let menit,
+date,
+seconds,
+jam,
+hari,
+tanggal,
+bulan,
+tahun;
+
+
+setInterval(function(){
+  date = new Date();
+  seconds = date.getSeconds();
+   menit = date.getMinutes();
+   jam = date.getHours();
+   hari = date.getDay();
+   tanggal = date.getDate();
+   bulan = date.getMonth();
+   tahun = date.getFullYear();
+  document.getElementById('date').innerHTML = tanggal+" "+((bulan+1)%12)+" "+tahun+"<br>"+jam+" : "+menit+" : "+seconds;
+  },1000);
